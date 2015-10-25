bb8349712133b86e165595b623235186f7afdeb0
ttakekawa <ttakekawa@manasoft.co.jp>
2015-10-25 21:28:28 +0900

    add single quart method

--- SlotGameLogicService/Program.cs
+++ SlotGameLogicService/Program.cs
@@ -146,16 +146,31 @@ class GameLogic
 static class stringExtension
 {
     /// <summary>
+    /// Single Quart
+    /// </summary>
+    /// <param name="source"></param>
+    /// <returns></returns>
+    static public string SQ(this string source)
+    {
+        return Quart(source, "'");
+    }
+
+    /// <summary>
     /// Double Quart
     /// </summary>
     /// <param name="source"></param>
     /// <returns></returns>
     static public string DQ(this string source)
     {
+        return Quart(source, @"""");
+    }
+
+    static string Quart(string source, string quart)
+    {
         var builder = new StringBuilder();
-        builder.Append(@"""");
+        builder.Append(quart);
         builder.Append(source);
-        builder.Append(@"""");
+        builder.Append(quart);
         return builder.ToString();
     }
 }
