772f888a3d84511817a6990d23cee2b351ab5235
tonitakekawa <tonitakekawa@gmail.com>
2015-10-27 13:17:45 +0900

    modify

--- SlotGameLogicService/Program.cs
+++ SlotGameLogicService/Program.cs
@@ -12,74 +12,52 @@ namespace SlotGameLogicService
     using UserId = String;
     using Json = String;
 
-class GameLogic
-{
+    class GameLogic
+    {
         TextWriter writer = Console.Out;
 
         Dictionary<Tuple<GameId, UserId>, IMachine> machines =
             new Dictionary<Tuple<GameId, UserId>, IMachine>();
 
-    static void Main(string[] args)
-    {
-        new GameLogic().Run();
-    }
-
-    void Run()
-    {
-        var listener = new HttpListener();
-
-        try
+        static void Main(string[] args)
         {
-            listener.Prefixes.Add("http://*:9876/");
-            listener.Start();
-        }
-        catch (Exception e)
-        {
-            writer.WriteLine("[ERROR]GAMELOGIC SERVER RUN FAILED");
-            writer.WriteLine(e);
-            return;
+            new GameLogic().Run();
         }
 
-        writer.WriteLine("[INFO]GAMELOGIC SERVER RUN PORT:9876");
+        void Run()
+        {
+            var listener = new HttpListener();
+
+            try
+            {
+                listener.Prefixes.Add("http://*:9876/");
+                listener.Start();
+            }
+            catch (Exception e)
+            {
+                writer.WriteLine("[ERROR]GAMELOGIC SERVER RUN FAILED");
+                writer.WriteLine(e);
+                return;
+            }
+
+            writer.WriteLine("[INFO]GAMELOGIC SERVER RUN PORT:9876");
 
             Func<Associative, Json> defaultResponse = (param) =>
-        {
+            {
                 param.Add("result", "error".DQ());
 
                 return "{" +
                        string.Join(",", param.Select(e => e.Key.DQ() + ":" + e.Value)) +
-                      "}";
-        };
-
-            Func<Associative, Json> configResponse = (param) =>
-        {
-                var gameId = null as GameId;
-                var userId = null as UserId;
-
-                try
-            {
-                new { key = "balance", value = "123.4" },
-                new { key = "setting", value = "1" },
-                new { key = "reelleft", value = "0" },
-                new { key = "reelcenter", value = "0" },
-                new { key = "reelright", value = "0" },
-                new { key = "seed", value = "0" },
+                       "}";
             };
 
-            var res = "{" +
-                      string.Join(",", table.Select(e => e.key.DQ() + ":" + e.value)) +
-                      "}";
-
-            return res;
-        };
-
-            Func<Associative, Json> initResponse = (param) =>
-        {
+            Func<Associative, Json> configResponse = (param) =>
+            {
                 var gameId = null as GameId;
                 var userId = null as UserId;
 
                 try
-            {
+                {
                     gameId = param["gameId"];
                     userId = param["userId"];
                 }
@@ -89,12 +67,61 @@ class GameLogic
                     return defaultResponse(param);
                 }
 
-            var res = "{" +
-                      string.Join(",", table.Select(e => e.key.DQ() + ":" + e.value)) +
-                      "}";
+                var machine = null as IMachine;
+                var key = Tuple.Create(gameId, userId);
 
-            return res;
-        };
+                if (machines.ContainsKey(key))
+                {
+                    machines.Remove(key);
+                }
+
+                machine = MachineFactory.Create(gameId);
+                machines.Add(key, machine);
+
+                var table = new[]
+                {
+                    new { key = "setting", value = "1" },
+                    new { key = "reelleft", value = "0" },
+                    new { key = "reelcenter", value = "0" },
+                    new { key = "reelright", value = "0" },
+                    new { key = "seed", value = "0" },
+                };
+
+                var res = "{" +
+                          string.Join(",", table.Select(e => e.key.DQ() + ":" + e.value)) +
+                          "}";
+
+                return res;
+            };
+
+            Func<Associative, Json> initResponse = (param) =>
+            {
+                var gameId = null as GameId;
+                var userId = null as UserId;
+
+                try
+                {
+                    gameId = param["gameId"];
+                    userId = param["userId"];
+                }
+                catch (Exception ex)
+                {
+                    writer.WriteLine(ex);
+                    return defaultResponse(param);
+                }
+
+                var machine = null as IMachine;
+                var key = Tuple.Create(gameId, userId);
+
+                if (!machines.ContainsKey(key))
+                {
+                    return defaultResponse(param);
+                }
+
+                var res = "{}";
+
+                return res;
+            };
 
             Func<Associative, Json> playResponse = (param) =>
             {
@@ -116,23 +143,22 @@ class GameLogic
                 var key = Tuple.Create(gameId, userId);
 
                 if (!machines.ContainsKey(key))
-        {
+                {
                     return defaultResponse(param);
                 }
 
-            var table = new[]
-            {
-                new { key = "balance", value = "123.4" },
-                new { key = "yaku", value = "1" },
-                new { key = "route", value = "2" },
+                var table = new[]
+                {
+                    new { key = "yaku", value = "1" },
+                    new { key = "route", value = "2" },
+                };
+
+                var res = "{" +
+                          string.Join(",", table.Select(e => e.key.DQ() + ":" + e.value)) +
+                          "}";
+
+                return res;
             };
-
-            var res = "{" +
-                      string.Join(",", table.Select(e => e.key.DQ() + ":" + e.value)) +
-                      "}";
-
-            return res;
-        };
 
             Func<Associative, Json> correctResponse = (param) =>
             {
@@ -154,51 +180,50 @@ class GameLogic
                 var key = Tuple.Create(gameId, userId);
 
                 if (!machines.ContainsKey(key))
-        {
+                {
                     return defaultResponse(param);
                 }
 
-            var table = new[]
-            {
-                new { key = "balance", value = "123.4" },
-                new { key = "result", value = "WIN".DQ() },
+                var table = new[]
+                {
+                    new { key = "result", value = "WIN".DQ() },
+                };
+
+                var res = "{" +
+                          string.Join(",", table.Select(e => e.key.DQ() + ":" + e.value)) +
+                          "}";
+
+                return res;
             };
 
-            var res = "{" +
-                      string.Join(",", table.Select(e => e.key.DQ() + ":" + e.value)) +
-                      "}";
-
-            return res;
-        };
-
             var pathTable = new[]
-        {
-            new { path = "/config", response = configResponse },
-            new { path = "/init", response = initResponse },
-            new { path = "/play", response = playResponse },
-            new { path = "/correct", response = correctResponse },
-        };
+            {
+                new { path = "/config", response = configResponse },
+                new { path = "/init", response = initResponse },
+                new { path = "/play", response = playResponse },
+                new { path = "/correct", response = correctResponse },
+            };
 
-        while (true)
-        {
-            var context = listener.GetContext();
-            var req = context.Request;
-            var res = context.Response;
+            while (true)
+            {
+                var context = listener.GetContext();
+                var req = context.Request;
+                var res = context.Response;
 
-            var url = req.Url;
-            var localPath = url.LocalPath;
+                var url = req.Url;
+                var localPath = url.LocalPath;
                 var reqparams = Query2KeyValues(url.Query);
 
-            var responseString = pathTable.Where(pt => pt.path == localPath)
+                var responseString = pathTable.Where(pt => pt.path == localPath)
                                               .Select(pt => pt.response(reqparams))
                                               .FirstOrDefault() ?? defaultResponse(reqparams);
 
-            var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
 
-            res.ContentLength64 = buffer.Length;
-            res.OutputStream.Write(buffer, 0, buffer.Length);
-            res.Close();
-        }
+                res.ContentLength64 = buffer.Length;
+                res.OutputStream.Write(buffer, 0, buffer.Length);
+                res.Close();
+            }
         }
 
         /// <summary>
@@ -219,44 +244,44 @@ class GameLogic
                      .ToList()
                      .ForEach(kv => result.Add(kv.Key, kv.Value));
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 writer.WriteLine(ex);
-    }
+            }
 
             return result;
         }
-}
-
-static class stringExtension
-{
-    /// <summary>
-    /// Single Quart
-    /// </summary>
-    /// <param name="source"></param>
-    /// <returns></returns>
-    static public string SQ(this string source)
-    {
-        return Quart(source, "'");
     }
 
-    /// <summary>
-    /// Double Quart
-    /// </summary>
-    /// <param name="source"></param>
-    /// <returns></returns>
-    static public string DQ(this string source)
+    static class stringExtension
     {
-        return Quart(source, @"""");
-    }
+        /// <summary>
+        /// Single Quart
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        static public string SQ(this string source)
+        {
+            return Quart(source, "'");
+        }
 
-    static string Quart(string source, string quart)
-    {
-        var builder = new StringBuilder();
-        builder.Append(quart);
-        builder.Append(source);
-        builder.Append(quart);
-        return builder.ToString();
+        /// <summary>
+        /// Double Quart
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        static public string DQ(this string source)
+        {
+            return Quart(source, @"""");
+        }
+
+        static string Quart(string source, string quart)
+        {
+            var builder = new StringBuilder();
+            builder.Append(quart);
+            builder.Append(source);
+            builder.Append(quart);
+            return builder.ToString();
+        }
     }
-}
 }
